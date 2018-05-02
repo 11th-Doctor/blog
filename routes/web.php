fc7b09cd4ddd3a5b692bf6b63e55bc43c0d6c0f9
Daryl Zhang <august9382@gmail.com>
2018-05-02 22:24:47 +0800

    Added comment

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('contact','PagesController@getContact');
+Route::get('contact','PagesController@getContact'); //(url,controller)
 Route::get('about','PagesController@getAbout');
 Route::get('/','PagesController@getIndex');
 Route::resource('posts','PostController');
