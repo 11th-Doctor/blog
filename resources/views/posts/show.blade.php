f0238d74626e758c26986a214cad020d390e4eab
Daryl Zhang <august9382@gmail.com>
2018-05-04 23:18:42 +0800

    created the tmies of the post and linls of edit and delete.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,32 @@
 	@section('title','| View Post')
 
 	@section('content')
-		<h1>{{$post->title}}</h1>
-		<p class="lead">{{$post->body}}</p>
+		<div class="row">
+			<div class="col-md-8">
+				<h1>{{$post->title}}</h1>
+				<p class="lead">{{$post->body}}</p>
+			</div>
+			<div class="col-md-4">
+				<div class="well">
+					<dl class="dl-horizontal">
+					  <dt>建立時間: </dt>
+					  <dd>{{date('Y-m-d-l G.i.s A',strtotime($post->created_at))}}</dd>
+					</dl>
+					<dl class="dl-horizontal">
+					  <dt>上次更新時間: </dt>
+					  <dd>{{date('Y-m-d-l G.i.s A',strtotime($post->created_at))}}</dd>
+					</dl>
+					<hr>
+					<div class="row">
+						<div class="col-md-6">
+							{!!Html::linkRoute('posts.edit','編輯',array($post->id),array('class' => 'btn btn-primary btn-block'))!!}
+						</div>
+						<div class="col-md-6">
+							{!!Html::linkRoute('posts.destroy','刪除',array($post->id),array('class' => 'btn btn-danger btn-block'))!!}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
 	@endsection
