330b991f55943743246c98320fb3303c404b8bc3
Daryl Zhang <august9382@gmail.com>
2018-05-05 00:51:19 +0800

    created the page of index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+
+@section('title','| All Posts')
+
+@section('content')
+	<div class="row">
+		<div class="col-md-10">
+			<h1>所有貼文</h1>
+		</div>
+		<div class="col-md-2">
+			<a href="#" class="btn btn-primary btn-lg btn-h1-spacing">建立新貼文</a>
+		</div>
+	</div>
+@endsection
