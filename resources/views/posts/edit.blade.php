6aeeab98a393ebc8e9826779feeeb5f08a2e292a
Daryl Zhang <august9382@gmail.com>
2018-05-07 00:50:31 +0800

    created the page of editing.

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('main')
+
+@section('title','| Edit')
+
+@section('content')
+
+	<div class="row">
+		{!!Form::model($post,['route' => ['posts.update',$post->id]])!!}
+		<div class="col-md-8">
+			{{Form::label('title','標題:')}}
+			{{Form::text('title',null,['class' => 'form-control input-lg'])}}
+			{{Form::label('body','本文:',['class' => 'form-spacing-top'])}}
+			{{Form::textarea('body',null,['class' => 'form-control'])}}
+		</div>
+		<div class="col-md-4">
+			<div class="well">
+				<dl class="dl-horizontal">
+				  <dt>建立時間: </dt>
+				  <dd>{{date('Y-m-d-l G.i.s A',strtotime($post->created_at))}}</dd>
+				</dl>
+
+				<dl class="dl-horizontal">
+				  <dt>上次更新時間: </dt>
+				  <dd>{{date('Y-m-d-l G.i.s A',strtotime($post->created_at))}}</dd>
+				</dl>
+				<hr>
+				<div class="row">
+					<div class="col-md-6">
+						{!!Html::linkRoute('posts.show','取消',array($post->id),array('class' => 'btn btn-danger btn-block'))!!}
+					</div>
+					<div class="col-md-6">
+						{!!Html::linkRoute('posts.update','儲存修改',array($post->id),array('class'=>'btn btn-success btn-block'))!!}
+					</div>
+				</div>
+			</div>
+		</div>
+		{!!Form::close()!!}
+	</div> <!--end of .row -->
+
+@endsection
