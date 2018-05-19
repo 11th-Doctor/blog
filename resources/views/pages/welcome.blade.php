35ac559d6a19d6da22498d4aee753ac267d289c2
Daryl Zhang <august9382@gmail.com>
2018-05-19 12:04:09 +0800

    modified welcome page to show the most recent four pages.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -20,37 +20,17 @@
 
     <div class="row">
         <div class="col-md-8">
+          
+          @foreach($posts as $post)
             <div class="post">
-              <h3>Post Title</h3>
+              <h3>{{$post->title}}</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ab optio nemo alias, voluptate sunt labore delectus culpa eligendi vitae, magni reiciendis tempora facilis maxime aperiam obcaecati sit, dolorem non?
+                {{substr($post->body,0,200)}} {{strlen($post->body) > 200 ? '...':''}}
               </p>
-              <a href="#" class="btn btn-primary">繼續閱讀...</a>
+              <a href="{{route('posts.show',[$post->id])}}" class="btn btn-primary">繼續閱讀...</a>
             </div>
+          @endforeach
 
-            <div class="post">
-              <h3>Post Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ab optio nemo alias, voluptate sunt labore delectus culpa eligendi vitae, magni reiciendis tempora facilis maxime aperiam obcaecati sit, dolorem non?
-              </p>
-              <a href="#" class="btn btn-primary">繼續閱讀...</a>
-            </div>
-
-            <div class="post">
-              <h3>Post Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ab optio nemo alias, voluptate sunt labore delectus culpa eligendi vitae, magni reiciendis tempora facilis maxime aperiam obcaecati sit, dolorem non?
-              </p>
-              <a href="#" class="btn btn-primary">繼續閱讀...</a>
-            </div>
-
-            <div class="post">
-              <h3>Post Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ab optio nemo alias, voluptate sunt labore delectus culpa eligendi vitae, magni reiciendis tempora facilis maxime aperiam obcaecati sit, dolorem non?
-              </p>
-              <a href="#" class="btn btn-primary">繼續閱讀...</a>
-            </div>
         </div>
         <div class="col-md-3 col-md-offset-1">
           <h2>Sidebar</h2>
